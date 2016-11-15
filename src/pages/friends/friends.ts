b8fe52eeb5a1397e7dea7d6c405514b770e4866d
Kimak <mickael.dumand@gmail.com>
2016-11-15 17:27:41 +0100

    add cordova contacts plugin

--- src/pages/friends/friends.ts
+++ src/pages/friends/friends.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Contacts} from 'ionic-native';
 
 /*
   Generated class for the Friends page.
@@ -13,10 +13,33 @@ import { NavController } from 'ionic-angular';
 })
 export class FriendsPage {
 
-  constructor(public navCtrl: NavController) {}
+    contactsfound:any;
 
-  ionViewDidLoad() {
-    console.log('Hello FriendsPage Page');
-  }
+    constructor() {
+
+        this.contactsfound = [];
+        /* Uncomment this mock data to avoid device building on test
+        let contacts:any = [{name:"Bob"},{name:"Bobby"},{name:"Charles"},{name:"Erik"},{name:"Amery"},{name:"Alhan"}];
+        setTimeout(()=>{
+          contacts.forEach( (c)=> {
+            if (c.name) {
+              this.contactsfound.push({name: c.name});
+            }
+          });
+        },1000)*/
+
+
+        Contacts.find(['*']).then((contacts) => {
+          contacts.forEach( (c)=> {
+            if (c.name.givenName && c.phoneNumbers) {
+              this.contactsfound.push({name: c.name.givenName, phone: c.phoneNumbers[0].value}); // grab only the properties you need avoiding birthday (ios bug on date formating) http://stackoverflow.com/questions/36798316/ionic-cordova-contacts-plugin-returns-invalid-date-on-ios-after-upgrade-to-ionic
+            }
+          });
+        },(error)=>{
+            alert("error "+error)
+        }).catch((error)=>{
+          alert("catch"+error)
+        })
+      }
 
 }
